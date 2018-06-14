4e22db34f3441ea8fd5db7b00072bd8bd7dbdcd3
Klaus Kazlauskas <klausmachado@gmail.com>
2018-06-14 05:27:36 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const emoji = require('node-emoji');
+
+const pkg = require('../package');
+const [,, args] = process.argv;
+
+program
+  .version(pkg.version, '-v, --version');
+
+program.parse(process.argv);
