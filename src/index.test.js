08b61f47f03ea9c126ab3a4086d982e8b93e4dc0
Klaus Kazlauskas <klausmachado@gmail.com>
2018-06-14 06:34:14 -0300

    test(ngr): add initial test and configuration

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+
+const pkg = require('../package');
+const ngr = './src/index.js';
+
+test('returns the package version', (done) => {
+  exec(`${ngr} --version`, (err, stdout, stderr) => {
+    if (err) throw stderr;
+    expect(stdout.replace('\n', '')).toBe(pkg.version);
+    done();
+  });
+});
